9623046654fe37b1d5e16e5545cad72ad74d308d
Dalton Wu <dalton.z.wu@gmail.com>
2014-10-01 08:47:25 -0400

    yeah

---
+++ 14-09-30/Driver.java
@@ -0,0 +1,14 @@
+public class Driver {
+    public static void main(String[] args) {
+	System.out.println("For loop:");
+	for(int i=0; i<10; i++) {
+	    System.out.println(i);
+	}
+	System.out.println("\nWhile loop:");
+	int foo = 0;
+	while(foo < 10) {
+	    System.out.println(foo);
+	    foo++;
+	}
+    }
+}
