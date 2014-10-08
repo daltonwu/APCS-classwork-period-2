e1bb010810dd8212da6001b2e9f8ebf3e1adf17d
Dalton Wu <dalton.z.wu@gmail.com>
2014-10-08 08:49:18 -0400

    COMMITTED!

---
+++ 14-10-07/Basechar.java
@@ -0,0 +1,7 @@
+public class Basechar {
+    private int health=20;
+        public int getHealth() {
+            return health;
+        }
+    }
+}
---
+++ 14-10-07/Warrior.java
@@ -0,0 +1,7 @@
+public class Warrior extends Basechar {
+    private int health = 50000;
+
+    public int getHealth() {
+        return health;
+    }
+}
